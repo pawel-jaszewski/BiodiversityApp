fcb0315e1d07b0746662c8b9ac21bda609f9451c
Paweł Jaszewski <jaszewskipawel1@gmail.com>
2022-11-24 07:38:18 +0100

    main add photos

--- server.R
+++ server.R
@@ -7,33 +7,54 @@ library(DT)
 server <- shinyServer(function(input, output, session) {
     
     vOccurencePoland <- fread("./occurencePoland.csv")
+    vMultimediaPoland <- fread("./multimediaPoland.csv")
     
-    updateSelectizeInput(session,
-                         inputId = "speciesSelector",
-                         choices = sort(c(unique(vOccurencePoland[, scientificName]),
-                                          unique(vOccurencePoland[vernacularName != "",
-                                                                  vernacularName]))),
-                         server = TRUE)
+    updateSelectizeInput(
+       session,
+       inputId = "speciesSelector",
+       choices = sort(c(unique(vOccurencePoland[, scientificName]),
+                        unique(vOccurencePoland[vernacularName != "",
+                                                vernacularName]))),
+       server = TRUE)
     
     vSelectedData <- reactive({
-        vOccurencePoland[scientificName == input$speciesSelector |
-                             vernacularName == input$speciesSelector,]
+      vOccurencePoland[scientificName == input$speciesSelector |
+                         vernacularName == input$speciesSelector,]
     })
     
     output$map <- renderLeaflet({
-        req(input$speciesSelector)
-        leaflet(vSelectedData()) %>% addTiles() %>% 
-            addMarkers(lng = ~longitudeDecimal,
-                       lat = ~latitudeDecimal,
-                       popup = ~paste(gsub("Poland - ", "", locality), eventDate, sep = "<br/>"))
+      req(input$speciesSelector)
+      leaflet(vSelectedData()) %>% addTiles() %>% 
+          addMarkers(lng = ~longitudeDecimal,
+                     lat = ~latitudeDecimal,
+                     popup = ~paste(gsub("Poland - ", "", locality), eventDate, sep = "<br/>"))
     })
     
     output$timePlot <- renderTimevis({
-        req(input$speciesSelector)
-        vSelectedDataDate <- data.frame(
-            content = vSelectedData()[, eventDate],
-            start = as.Date(vSelectedData()[, eventDate], format = "%d.%m.%Y"))
-        timevis(vSelectedDataDate, options = list(showCurrentTime = FALSE))
+      req(input$speciesSelector)
+      vSelectedDataDate <- data.frame(
+          content = vSelectedData()[, eventDate],
+          start = as.Date(vSelectedData()[, eventDate], format = "%d.%m.%Y"))
+      timevis(vSelectedDataDate, options = list(showCurrentTime = FALSE))
+    })
+    
+    output$photos <- renderUI({
+      req(input$speciesSelector)
+      fluidRow(
+        lapply(unique(vMultimediaPoland[CoreId %in% vSelectedData()$id, CoreId]),
+               function(x){
+                 column(
+                   width = 3,
+                   HTML(paste(vSelectedData()[id == x, eventDate],
+                              paste0("<img src='",
+                                     vMultimediaPoland[CoreId == x, accessURI],
+                                     "', width = '200', height = '200'>"),
+                              sep = "<br/>")
+                  )
+                )
+              }
+        )
+      )
     })
     
     orderedSpecies("orderedSpecies")
--- ui.R
+++ ui.R
@@ -4,26 +4,27 @@ library(DT)
 
 ui <- shinyUI(
     fluidPage(
-        titlePanel("Global Biodiversity Information Facility"),
-        sidebarLayout(
-            sidebarPanel(
-                id = "sidebar",
-                selectizeInput(inputId = "speciesSelector",
-                               label = "Choose species by either Scientific or Vernacular Name
-                               to be presented on the map and the timeline:",
-                               choices = NULL),
-                br(),
-                orderedSpeciesUI("orderedSpecies")
-            ),
-            mainPanel(
-                leafletOutput("map"),
-                timevisOutput("timePlot")
-            )
+      titlePanel("Global Biodiversity Information Facility"),
+      sidebarLayout(
+        sidebarPanel(
+          id = "sidebar",
+          selectizeInput(inputId = "speciesSelector",
+                         label = "Choose species by either Scientific or Vernacular Name
+                         to be presented on the map and the timeline:",
+                         choices = NULL),
+          br(),
+          orderedSpeciesUI("orderedSpecies")
         ),
-        tags$head(tags$style(
-            HTML('
-            #sidebar {background-color: #FFFFFF;}
-            ')
-        ))
+        mainPanel(
+          leafletOutput("map"),
+          timevisOutput("timePlot"),
+          uiOutput("photos")
+        )
+      ),
+      tags$head(tags$style(
+        HTML('
+        #sidebar {background-color: #FFFFFF;}
+        ')
+      ))
     )
 )
